104bccf66e71b25f710491e6646762ba96c86b45
olena-dev <leno4ka.taras@gmail.com>
2021-12-28 01:54:26 -0700

    added eslint

--- src/app/services/request-data.service.ts
+++ src/app/services/request-data.service.ts
@@ -1,24 +1,47 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Inject, Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, of } from "rxjs";
 import * as countriesData from '../../assets/countries.json';
 import * as citiesData from '../../assets/cities.json';
 import * as airportsData from '../../assets/airports.json';
+import { DOCUMENT } from "@angular/common";
+import { take } from 'rxjs/operators';
 
 @Injectable()
 export class RequestDataService {
-  constructor(private httpClient: HttpClient) {
+  baseUrl: string;
+  constructor(private httpClient: HttpClient, @Inject(DOCUMENT) private document: Document) {
+    this.baseUrl = this.document.location.origin;
   }
 
   getCountriesData(): Observable<any> {
-    return of(countriesData)
+    return of(countriesData).pipe(
+      take(1)
+    )
   }
 
   getCitiesData(): Observable<any> {
-    return of(citiesData)
+    return of(citiesData).pipe(
+      take(1)
+    )
   }
 
   getAirportsData(): Observable<any> {
-    return of(airportsData)
+    return of(airportsData).pipe(
+      take(1)
+    )
   }
+
+  // exampleRequestGetChipTickets() {
+  //   const headerDict = {
+  //     'Access-Control-Allow-Origin': '*',
+  //     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  //     'x-access-token': 'a178f5b65a824a9f54d30f2f37421ac2'
+  //   }
+  //
+  //   const requestOptions = {
+  //     headers: new HttpHeaders(headerDict)
+  //   };
+  //   return this.httpClient.request('get', this.baseUrl + '/prices/cheap?origin=LWO&destination=HKT&token=PutYourTokenHere', requestOptions)
+  // }
 }
