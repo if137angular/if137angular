a569e40f1c42f58452fe69ce85d877d0157ec0d3
RomanMaruk <maruchok2@gmail.com>
2022-01-05 12:40:35 +0200

    delete some spase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { FirstComponent } from './components/forTest/first/first.component';
-import { SecondComponent } from './components/forTest/second/second.component';
-import { ThirdComponent } from './components/forTest/third/third.component';
+import { FirstComponent } from './components/forTest/first/first.component'; // only for test, delete
+import { SecondComponent } from './components/forTest/second/second.component'; // only for test, delete
+import { ThirdComponent } from './components/forTest/third/third.component'; // only for test, delete
 
 export const routes: Routes = [
-  {path: '', component: FirstComponent, data: {tab: 'First'}},
-  {path: 'second', component: SecondComponent, data: {tab: 'Second'}},
-  {path: 'third', component: ThirdComponent, data: {tab: 'Third'}}
+  {path: '', component: FirstComponent, data: {tab: 'First'}}, // only for test, delete
+  {path: 'second', component: SecondComponent, data: {tab: 'Second'}}, // only for test, delete
+  {path: 'third', component: ThirdComponent, data: {tab: 'Third'}} // only for test, delete
 ];
 
 @NgModule({
