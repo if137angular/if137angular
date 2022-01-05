4176f5b374087c4f4b62b8c392e1c350f11891d9
Vadym <62544653+icenfame@users.noreply.github.com>
2022-01-05 15:43:54 +0200

    refactor: app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { NgxsModule } from '@ngxs/store';
+import { AppState } from 'src/app/store/app.state';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -9,31 +17,23 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RequestDataService } from 'src/app/services/request-data.service';
+import { FlightsInfoService } from 'src/app/services/flights-info.service';
+
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxsModule } from "@ngxs/store";
-
-import { AppState } from "src/app/store/app.state";
-import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
-import { RequestDataService } from "src/app/services/request-data.service";
-import { HttpClientModule } from "@angular/common/http";
 import { FlightDataFormComponent } from './components/flight-data-form/flight-data-form.component';
 import { AutocompleteInputComponent } from './components/autocomplete-input/autocomplete-input.component';
 import { ButtonComponent } from './components/button/button.component';
 import { DatepickerComponent } from './components/datepicker/datepicker.component';
 import { TransfersComponent } from './components/transfers/transfers.component';
-
-import { MatTabsModule } from '@angular/material/tabs'; // this import for tabs material
-
 import { NavComponent } from './components/nav/nav.component';
-
-// code below only for testing
 import { FirstComponent } from './components/forTest/first/first.component';
 import { SecondComponent } from './components/forTest/second/second.component';
 import { ThirdComponent } from './components/forTest/third/third.component';
-import { FlightsInfoService } from "src/app/services/flights-info.service";
+import { AutocompleteComponent } from './components/autocomplete/autocomplete.component';
 
 @NgModule({
   declarations: [
@@ -46,14 +46,18 @@ import { FlightsInfoService } from "src/app/services/flights-info.service";
     NavComponent,
     FirstComponent,
     SecondComponent,
-    ThirdComponent
+    ThirdComponent,
+    AutocompleteComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatAutocompleteModule,
+    HttpClientModule,
+    FormsModule,
     ReactiveFormsModule,
+
+    MatAutocompleteModule,
     MatInputModule,
     MatButtonModule,
     MatDatepickerModule,
@@ -61,14 +65,15 @@ import { FlightsInfoService } from "src/app/services/flights-info.service";
     MatRadioModule,
     MatSelectModule,
     MatCheckboxModule,
-    HttpClientModule,
+    MatTabsModule,
+    MatFormFieldModule,
+
     NgxsModule.forRoot(AppState, {
-      developmentMode: true
+      developmentMode: true,
     }),
     NgxsLoggerPluginModule.forRoot(),
-    MatTabsModule
   ],
   providers: [RequestDataService, FlightsInfoService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
