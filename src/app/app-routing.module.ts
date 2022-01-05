cc99702e5174f20d900cfbdee0e684e71b83d0e8
Andrii Miliuchenko <andrii2003@gmail.com>
2022-01-05 18:13:44 +0200

    add calendar to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './components/forTest/first/first.component';
 import { SecondComponent } from './components/forTest/second/second.component';
 import { ThirdComponent } from './components/forTest/third/third.component';
+import { CalendarOfPricesComponent } from './components/calendar-of-prices/calendar-of-prices.component';
 
 export const routes: Routes = [
   { path: '', component: FirstComponent, data: { tab: 'First' } },
   { path: 'second', component: SecondComponent, data: { tab: 'Second' } },
   { path: 'third', component: ThirdComponent, data: { tab: 'Third' } },
+  {
+    path: 'calendar',
+    component: CalendarOfPricesComponent,
+    data: { tab: 'Calendar of prices' },
+  },
 ];
 
 @NgModule({
