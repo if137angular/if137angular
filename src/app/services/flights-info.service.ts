e73e6b7818af0df56885b388d3cd3aa3ac102f7d
Vadym <62544653+icenfame@users.noreply.github.com>
2022-01-07 12:23:36 +0200

    fix: week-matrix request

--- src/app/services/flights-info.service.ts
+++ src/app/services/flights-info.service.ts
@@ -1,5 +1,4 @@
 import { Inject, Injectable } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -7,44 +6,31 @@ import { Observable } from 'rxjs';
 export class FlightsInfoService {
   baseUrl: string;
 
-  constructor(
-    @Inject(DOCUMENT) private document: Document,
-    private httpClient: HttpClient
-  ) {
-    this.baseUrl = this.document.location.origin;
-  }
+  constructor(private http: HttpClient) {}
 
   exampleRequestGetChipTickets(): Observable<any> {
     const headerDict = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
       'x-access-token': 'PutYourTokenHere',
     };
 
     const requestOptions = {
       headers: new HttpHeaders(headerDict),
     };
-    return this.httpClient.request(
-      'get',
-      this.baseUrl +
-        '/v1/prices/cheap?origin=LWO&destination=HKT&token=PutYourTokenHere',
+    return this.http.get(
+      '/v2/prices/cheap?origin=LWO&destination=HKT&token=PutYourTokenHere',
       requestOptions
     );
   }
   RequestGetCalendarOfPrices() {
     const headerDict = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
       'x-access-token': '51b362c72de38be9bcfdc31c8339c019',
     };
 
     const requestOptions = {
       headers: new HttpHeaders(headerDict),
     };
-    return this.httpClient.request(
-      'get',
-      this.baseUrl +
-        '/prices/week-matrix?currency=usd&origin=LED&destination=HKT&show_to_affiliates=true&depart_date=2022-01-17&return_date=2022-01-24&token=51b362c72de38be9bcfdc31c8339c019',
+    return this.http.get(
+      '/v2/prices/week-matrix?currency=usd&origin=LED&destination=HKT&show_to_affiliates=true&depart_date=2022-01-17&return_date=2022-01-24&token=51b362c72de38be9bcfdc31c8339c019',
       requestOptions
     );
   }
