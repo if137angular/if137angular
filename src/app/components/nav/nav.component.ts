1ce9353f1f12fc6d2e0c8834f55ea00b2153bdc7
Vadym <62544653+icenfame@users.noreply.github.com>
2022-01-07 13:37:07 +0200

    fix: routing 404 error on refresh

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { routes } from 'src/app/app-routing.module';
-
+import { PlatformLocation } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
-  
-    links: any = routes
-  
-    activeLink = this.links[0].data.tab;
+  links: any = routes;
+  activeLink = this.pLocation.hash.slice(2);
 
+  constructor(private pLocation: PlatformLocation) {}
 }
