d0fc6a27fc01b9ca010bb5040b8e24ec9ab9a5be
Vadym <62544653+icenfame@users.noreply.github.com>
2022-01-10 14:08:58 +0200

    fix: get week-matrix

--- src/app/services/flights-info.service.ts
+++ src/app/services/flights-info.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { GetCalendarOfPricesRequest } from '../components/calendar-of-prices/calendar-of-prices.component';
 
 @Injectable()
 export class FlightsInfoService {
@@ -27,7 +28,7 @@ export class FlightsInfoService {
     const requestOptions = {
       headers: new HttpHeaders(headerDict),
     };
-    return this.http.get(
+    return this.http.get<GetCalendarOfPricesRequest>(
       '/v2/prices/week-matrix?currency=usd&origin=LED&destination=HKT&show_to_affiliates=true&depart_date=2022-01-17&return_date=2022-01-24&token=51b362c72de38be9bcfdc31c8339c019',
       requestOptions
     );
