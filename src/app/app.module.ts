9fd89bc94e646ff24c0d7dd1063965560908678e
RomanMaruk <maruchok2@gmail.com>
2022-01-11 13:15:10 +0200

    delete tests component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,16 +30,11 @@ import { AppComponent } from './app.component';
 import { FlightDataFormComponent } from './components/flight-data-form/flight-data-form.component';
 import { ButtonComponent } from './components/button/button.component';
 import { NavComponent } from './components/nav/nav.component';
-import { FirstComponent } from './components/forTest/first/first.component';
-import { SecondComponent } from './components/forTest/second/second.component';
-import { ThirdComponent } from './components/forTest/third/third.component';
 import { AutocompleteComponent } from './components/autocomplete/autocomplete.component';
 import { CalendarOfPricesItemComponent } from './components/calendar-of-prices/calendar-of-prices-item/calendar-of-prices-item.component';
 import { CalendarOfPricesComponent } from './components/calendar-of-prices/calendar-of-prices.component';
 import { SpecialOffersComponent } from './components/special-offers/special-offers.component';
-import { NonStopTicketsComponent } from './components/non-stop-tickets/non-stop-tickets.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
-
 
 @NgModule({
   declarations: [
@@ -50,11 +45,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     CalendarOfPricesItemComponent,
     ButtonComponent,
     NavComponent,
-    FirstComponent,
-    SecondComponent,
-    ThirdComponent,
-    SpecialOffersComponent,
-    NonStopTicketsComponent
+    SpecialOffersComponent
   ],
   imports: [
     MatIconModule,
