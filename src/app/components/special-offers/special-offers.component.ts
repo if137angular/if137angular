444b6bc7da39315f37446fa8a18b34c92149ccd7
olena-dev <leno4ka.taras@gmail.com>
2022-01-16 04:14:17 -0700

    added dispatching special offers

--- src/app/components/special-offers/special-offers.component.ts
+++ src/app/components/special-offers/special-offers.component.ts
@@ -1,24 +1,32 @@
 import { RequestDataState } from 'src/app/store/request-data.state';
 import { Select, Store } from '@ngxs/store';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { FlightsInfoService } from 'src/app/services/flights-info.service';
+import { FormDataModel } from "src/app/models/formData.model";
+import { GetSpecialOffers } from "src/app/store/flight-info.action";
+import { FlightInfoState } from "src/app/store/flight-info.state";
 
 @Component({
   selector: 'app-special-offers',
   templateUrl: './special-offers.component.html',
-  styleUrls: ['./special-offers.component.scss']
+  styleUrls: [ './special-offers.component.scss' ]
 })
 export class SpecialOffersComponent implements OnInit {
 
   @Select(RequestDataState.formData)
-
   formData$: Observable<any>;
-  offers$: Observable<{ data: any }>;
+
+  @Select(FlightInfoState.specialOffers)
+  offers$: Observable<any>;
+
 
   language: string = 'en';
   currency: string = 'usd';
-  constructor(public flightsInfoService: FlightsInfoService) { }
+  cityOrigin: string = 'IEV';
+
+  constructor(public store: Store) {
+  }
 
   gotToLink(link: any) {
     window.open(`https://search.jetradar.com/flights/${link}&currency=${this.currency}&locale=${this.language}`, '_blank')
@@ -26,7 +34,10 @@ export class SpecialOffersComponent implements OnInit {
 
   getCurrency(number: any) {
     let language = this.language;
-    return new Intl.NumberFormat(language.substring(0, 2), { style: 'currency', currency: this.currency }).format(number);
+    return new Intl.NumberFormat(language.substring(0, 2), {
+      style: 'currency',
+      currency: this.currency
+    }).format(number);
   }
 
   getHours(min: any) {
@@ -36,29 +47,29 @@ export class SpecialOffersComponent implements OnInit {
   }
 
   ngOnInit(language = 'en', currency = 'eur', cityOrign: string = 'IEV', cityDestination: string = ''): void {
-    this.formData$.subscribe((formData: any) => {
-
-      // if (!formData) {
-      //   this.offers$ = this.flightsInfoService.getSpecialOffers(cityOrign, cityDestination, language, currency)
-      // } else {
-      //   this.offers$ = this.flightsInfoService.getSpecialOffers(formData.destinationFrom.code, cityDestination, language, currency)
-      // }
-
-      this.offers$ = this.flightsInfoService.getSpecialOffers(
-        formData.destinationFrom ? formData.destinationFrom.code : cityOrign,
-        formData.destinationTo ? formData.destinationTo.code : cityDestination,
-        language, currency);
-
+    this.dispatchSpecialOffers(this.store.selectSnapshot(RequestDataState.formData));
+    this.formData$.subscribe((formData: FormDataModel) => {
+      this.dispatchSpecialOffers(formData);
     });
   }
 
   onSelectedLanguageChanged(language: string) {
     this.language = language;
-    this.ngOnInit(language, this.currency);
+    this.dispatchSpecialOffers(this.store.selectSnapshot(RequestDataState.formData));
   }
 
   onSelectedCurrencyChanged(currency: string) {
     this.currency = currency;
-    this.ngOnInit(this.language, currency);
+    this.dispatchSpecialOffers(this.store.selectSnapshot(RequestDataState.formData));
+  }
+
+  dispatchSpecialOffers(formData: FormDataModel): void {
+    const payload = {
+      cityOrigin: formData.destinationFrom ? formData.destinationFrom.code : this.cityOrigin,
+      cityDestination: formData.destinationTo ? formData.destinationTo.code : '',
+      language: this.language,
+      currency: this.currency
+      }
+    this.store.dispatch(new GetSpecialOffers(payload))
   }
 }
