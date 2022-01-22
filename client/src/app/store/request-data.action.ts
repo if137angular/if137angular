859d292892af4ac3e10b58c796cbfa7f6fd3271c
Andrii Miliuchenko <andrii2003@gmail.com>
2022-01-22 11:05:14 +0200

    connect user data from ip to sotre

--- client/src/app/store/request-data.action.ts
+++ client/src/app/store/request-data.action.ts
@@ -1,53 +1,57 @@
+import { IpFullModel } from '../models/ip.model';
+
 //-- Request State Actions --
 export class GetCountries {
   static readonly type = '[Request] Get All Countries Data';
-  constructor(public payload: any = null) { }
+  constructor(public payload: any = null) {}
 }
 
 export class GetLocation {
   static readonly type = '[Request] Get Location Data';
-  constructor(public payload: any = null) { }
+  constructor(public payload: any = null) {}
 }
 
 export class GetCities {
   static readonly type = '[Request] Get All Cities Data';
-  constructor(public payload: any = null) { }
+  constructor(public payload: any = null) {}
 }
 
 export class GetAirports {
   static readonly type = '[Request] Get All Airports Data';
-  constructor(public payload: any = null) { }
+  constructor(public payload: any = null) {}
 }
-
 
 export class GetAirlines {
   static readonly type = '[Request] Get All Airlines Data';
 
-  constructor(public payload: any = null) {
-  }
+  constructor(public payload: any = null) {}
 }
 
 export class GetCurrencies {
   static readonly type = '[Request] Get All Currencies Data';
-  constructor(public payload: any = null) {
-  }
+  constructor(public payload: any = null) {}
 }
 
 export class GetLanguages {
   static readonly type = '[Request] Get All Languages Data';
-  constructor(public payload: any = null) {
-  }
+  constructor(public payload: any = null) {}
 }
 
 export class SetFormDate {
   static readonly type = '[Request] Set Form Data';
-  constructor(public payload: any) { }
+  constructor(public payload: any) {}
+}
+
+export class SetUserData {
+  static readonly type = '[Request] Set User Data from Ip';
+  constructor(public payload: IpFullModel) {}
 }
 
 export type RequestDataActions =
-  GetCountries |
-  GetCities |
-  GetAirports |
-  GetAirlines |
-  GetCurrencies |
-  GetLanguages;
+  | GetCountries
+  | GetCities
+  | GetAirports
+  | GetAirlines
+  | GetCurrencies
+  | GetLanguages
+  | SetUserData;
