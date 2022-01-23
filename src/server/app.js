040b33601efd88bb0267acb1a8ad1d31a7d277ee
Vadym <62544653+icenfame@users.noreply.github.com>
2022-01-23 13:08:10 +0200

    refactor!: project structure && server

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,10 @@
 const express = require("express");
+const app = express();
 const logger = require("morgan");
 const cors = require("cors");
 
-const favoritesRouter = require("./routes/api/favorites");
-
 const routes = require("./routes");
-
-const app = express();
+const favoritesRouter = require("./routes/api/favorites");
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -15,7 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/favorites", favoritesRouter);
-
 app.use("/", routes);
 
 app.get("/", (req, res) => {
