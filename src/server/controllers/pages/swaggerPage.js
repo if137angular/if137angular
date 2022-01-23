91c0db5863ddf4dee21724bc1f1e0aa53ae18763
Vadym <62544653+icenfame@users.noreply.github.com>
2022-01-23 15:09:06 +0200

    feat!: add proxy & static server

--- src/server/controllers/pages/swaggerPage.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-const swaggerPage = (_, res) => {
-  const mainPage = path.join(__dirname, "../../routes/index.html");
-  return res.sendFile(swaggerPage);
-};
-
-module.exports = swaggerPage;
