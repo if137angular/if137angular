40410228bb68c9f2a38884178ce7a640fbdd2648
Vadym <62544653+icenfame@users.noreply.github.com>
2022-01-27 18:31:47 +0200

    feat: redesign select currency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatMenuModule } from '@angular/material/menu';
 
 // Services
 import { RequestDataService } from 'src/app/services/request-data.service';
@@ -53,9 +54,9 @@ import { TransfersComponent } from './components/transfers/transfers.component';
 import { FlightTicketsForSpecialDatesComponent } from './components/flight-tickets-for-special-dates/flight-tickets-for-special-dates.component';
 
 import { SpecialOffersSelectComponent } from './components/special-offers/special-offers-select/special-offers-select.component';
-import { CheapestTicketsComponentOld} from "./components/cheapest-tickets-old-v/cheapest-tickets.component";
-import { TicketItemComponent} from "./components/cheapest-tickets-old-v/cheapest-ticket-item/ticket-item.component";
-import { FlightItemComponent} from "./components/cheapest-tickets-old-v/cheapest-ticket-item/flight-item/flight-item.component";
+import { CheapestTicketsComponentOld } from './components/cheapest-tickets-old-v/cheapest-tickets.component';
+import { TicketItemComponent } from './components/cheapest-tickets-old-v/cheapest-ticket-item/ticket-item.component';
+import { FlightItemComponent } from './components/cheapest-tickets-old-v/cheapest-ticket-item/flight-item/flight-item.component';
 import { CityDestinationComponent } from './components/city-destination/city-destination.component';
 import { FlightTicketComponent } from 'src/app/components/flight-tickets-for-special-dates/flight-ticket/flight-ticket.component';
 
@@ -70,7 +71,6 @@ import { MapsComponent } from './components/maps/maps.component';
 import { NoRecordsFoundComponent } from './components/no-records-found/no-records-found.component';
 import { CurrencyDropdownComponent } from './components/currency-dropdown/currency-dropdown.component';
 import { CheapestTicketsComponent } from './components/cheapest-tickets/cheapest-tickets.component';
-
 
 @NgModule({
   declarations: [
@@ -116,6 +116,7 @@ import { CheapestTicketsComponent } from './components/cheapest-tickets/cheapest
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+
     MatSliderModule,
     MatSelectModule,
     MatAutocompleteModule,
@@ -136,6 +137,8 @@ import { CheapestTicketsComponent } from './components/cheapest-tickets/cheapest
     MatProgressSpinnerModule,
     MatToolbarModule,
     MatProgressBarModule,
+    MatMenuModule,
+
     NgxsModule.forRoot(appState, {
       developmentMode: true,
     }),
@@ -149,7 +152,7 @@ import { CheapestTicketsComponent } from './components/cheapest-tickets/cheapest
       provide: LOCALE_ID,
       useValue: 'en-GB',
     },
-    { provide: 'Window',  useValue: window }
+    { provide: 'Window', useValue: window },
   ],
   bootstrap: [AppComponent],
 })
