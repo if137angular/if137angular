e5b2c11eb3210d8b82333991a5ed05a3f799ac23
Andrii Miliuchenko <andrii2003@gmail.com>
2022-02-03 10:05:07 +0200

    refactor calendar

--- src/app/components/calendar-of-prices/calendar-of-prices.component.ts
+++ src/app/components/calendar-of-prices/calendar-of-prices.component.ts
@@ -6,88 +6,21 @@ import {
   CalendarOfPricesPayload,
 } from 'src/app/models/calendar-of-prices.model';
 import { FormDataModel } from 'src/app/models/formData.model';
-import {
-  CalendarOfPricesLoaded,
-  StartLoading,
-} from 'src/app/store/flight-info.action';
+import { CalendarOfPricesLoaded } from 'src/app/store/flight-info.action';
 import { FlightInfoState } from 'src/app/store/flight-info.state';
 import { RequestDataState } from 'src/app/store/request-data.state';
-
-// @Component({
-//   selector: 'app-calendar-of-prices',
-//   templateUrl: './calendar-of-prices.component.html',
-//   styleUrls: ['./calendar-of-prices.component.scss'],
-//   host: {
-//     class: 'calendar-component',
-//   },
-// })
-// export class CalendarOfPricesComponent implements OnInit {
-//   calendarData: CalendarOfPricesModel[];
-//   formData: CalendarOfPricesPayload;
-//   currency: string;
-//   loading: boolean;
-//   loadingCardCount: number[];
-
-//   constructor(private store: Store) {
-//     this.loadingCardCount = Array(36).map((n) => n);
-//   }
-
-//   ngOnInit(): void {
-//     this.store
-//       .select(FlightInfoState.calendarOfPrices)
-//       .subscribe((state) => (this.calendarData = state));
-
-//     this.store
-//       .select(FlightInfoState.currency)
-//       .subscribe((state) => (this.currency = state));
-
-//     this.store
-//       .select(FlightInfoState.loading)
-//       .subscribe((state) => (this.loading = state));
-
-//     this.store
-//       .select(RequestDataState.formData)
-//       .pipe(
-//         map((state: FormDataModel) => ({
-//           origin: state.destinationFrom.name,
-//           destination: state.destinationTo.name,
-//           originCode: state.destinationFrom.code,
-//           destinationCode: state.destinationTo.code,
-//           return_date: state.endDate.toISOString().split('T')[0],
-//           depart_date: state.startDate.toISOString().split('T')[0],
-//         }))
-//       )
-//       .subscribe((data: CalendarOfPricesPayload) => {
-//         this.store.dispatch([
-//           new StartLoading(),
-//           new CalendarOfPricesLoaded(data),
-//         ]);
-//         this.formData = data;
-//       });
-//   }
-// }
 import {
   Component,
   ChangeDetectionStrategy,
   ViewChild,
   TemplateRef,
 } from '@angular/core';
-import {
-  startOfDay,
-  endOfDay,
-  subDays,
-  addDays,
-  endOfMonth,
-  isSameDay,
-  isSameMonth,
-  addHours,
-} from 'date-fns';
+import { isSameDay, isSameMonth } from 'date-fns';
 import { Subject } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import {
   CalendarEvent,
-  CalendarEventAction,
   CalendarEventTimesChangedEvent,
   CalendarView,
 } from 'angular-calendar';
@@ -101,12 +34,15 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
   templateUrl: './calendar-of-prices.component.html',
 })
 export class CalendarOfPricesComponent implements OnInit {
+  @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any>;
   calendarData: CalendarOfPricesModel[];
   formData: CalendarOfPricesPayload;
-  currency: string;
-  loadingCardCount: number[];
 
-  constructor(private store: Store, private cdRef: ChangeDetectorRef) {}
+  constructor(
+    private store: Store,
+    private modal: NgbModal,
+    private cdRef: ChangeDetectorRef
+  ) {}
 
   events: any[];
 
@@ -115,11 +51,6 @@ export class CalendarOfPricesComponent implements OnInit {
       .select(FlightInfoState.calendarOfPrices)
       .pipe(untilDestroyed(this))
       .subscribe((state) => (this.events = state));
-
-    this.store
-      .select(FlightInfoState.currency)
-      .pipe(untilDestroyed(this))
-      .subscribe((state) => (this.currency = state));
 
     this.store
       .select(RequestDataState.formData)
@@ -139,8 +70,6 @@ export class CalendarOfPricesComponent implements OnInit {
         this.store.dispatch([new CalendarOfPricesLoaded(data)]);
         this.formData = data;
       });
-
-    this.cdRef.detectChanges();
   }
 
   view: CalendarView = CalendarView.Month;
@@ -151,7 +80,7 @@ export class CalendarOfPricesComponent implements OnInit {
 
   modalData: {
     action: string;
-    event: CalendarEvent;
+    event: any;
   };
 
   refresh = new Subject<void>();
@@ -190,7 +119,10 @@ export class CalendarOfPricesComponent implements OnInit {
     this.handleEvent('Dropped or resized', event);
   }
 
-  handleEvent(action: string, event: CalendarEvent): void {}
+  handleEvent(action: string, event: CalendarEvent): void {
+    this.modalData = { event, action };
+    this.modal.open(this.modalContent, { size: 'lg' });
+  }
 
   setView(view: CalendarView) {
     this.view = view;
@@ -200,3 +132,205 @@ export class CalendarOfPricesComponent implements OnInit {
     this.activeDayIsOpen = false;
   }
 }
+// import {
+//   Component,
+//   ChangeDetectionStrategy,
+//   ViewChild,
+//   TemplateRef,
+// } from '@angular/core';
+// import {
+//   startOfDay,
+//   endOfDay,
+//   subDays,
+//   addDays,
+//   endOfMonth,
+//   isSameDay,
+//   isSameMonth,
+//   addHours,
+// } from 'date-fns';
+// import { Subject } from 'rxjs';
+// import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+// import {
+//   CalendarEvent,
+//   CalendarEventAction,
+//   CalendarEventTimesChangedEvent,
+//   CalendarView,
+// } from 'angular-calendar';
+
+// const colors: any = {
+//   red: {
+//     primary: '#ad2121',
+//     secondary: '#FAE3E3',
+//   },
+//   blue: {
+//     primary: '#1e90ff',
+//     secondary: '#D1E8FF',
+//   },
+//   yellow: {
+//     primary: '#e3bc08',
+//     secondary: '#FDF1BA',
+//   },
+// };
+
+// @Component({
+//   selector: 'app-calendar-of-prices',
+//   changeDetection: ChangeDetectionStrategy.OnPush,
+//   styles: [
+//     `
+//       h3 {
+//         margin: 0 0 10px;
+//       }
+
+//       pre {
+//         background-color: #f5f5f5;
+//         padding: 15px;
+//       }
+//     `,
+//   ],
+//   templateUrl: './calendar-of-prices.component.html',
+// })
+// export class CalendarOfPricesComponent {
+//   @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any>;
+
+//   view: CalendarView = CalendarView.Month;
+
+//   CalendarView = CalendarView;
+
+//   viewDate: Date = new Date();
+
+//   modalData: {
+//     action: string;
+//     event: CalendarEvent;
+//   };
+
+//   actions: CalendarEventAction[] = [
+//     {
+//       label: '<i class="fas fa-fw fa-pencil-alt"></i>',
+//       a11yLabel: 'Edit',
+//       onClick: ({ event }: { event: CalendarEvent }): void => {
+//         this.handleEvent('Edited', event);
+//       },
+//     },
+//     {
+//       label: '<i class="fas fa-fw fa-trash-alt"></i>',
+//       a11yLabel: 'Delete',
+//       onClick: ({ event }: { event: CalendarEvent }): void => {
+//         this.events = this.events.filter((iEvent) => iEvent !== event);
+//         this.handleEvent('Deleted', event);
+//       },
+//     },
+//   ];
+
+//   refresh = new Subject<void>();
+
+//   events: CalendarEvent[] = [
+//     {
+//       start: subDays(startOfDay(new Date()), 1),
+//       end: addDays(new Date(), 1),
+//       title: 'A 3 day event',
+//       color: colors.red,
+//       actions: this.actions,
+//       allDay: true,
+//       resizable: {
+//         beforeStart: true,
+//         afterEnd: true,
+//       },
+//       draggable: true,
+//     },
+//     {
+//       start: startOfDay(new Date()),
+//       title: 'An event with no end date',
+//       color: colors.yellow,
+//       actions: this.actions,
+//     },
+//     {
+//       start: subDays(endOfMonth(new Date()), 3),
+//       end: addDays(endOfMonth(new Date()), 3),
+//       title: 'A long event that spans 2 months',
+//       color: colors.blue,
+//       allDay: true,
+//     },
+//     {
+//       start: addHours(startOfDay(new Date()), 2),
+//       end: addHours(new Date(), 2),
+//       title: 'A draggable and resizable event',
+//       color: colors.yellow,
+//       actions: this.actions,
+//       resizable: {
+//         beforeStart: true,
+//         afterEnd: true,
+//       },
+//       draggable: true,
+//     },
+//   ];
+
+//   activeDayIsOpen: boolean = true;
+
+//   constructor(private modal: NgbModal) {}
+
+//   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+//     if (isSameMonth(date, this.viewDate)) {
+//       if (
+//         (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+//         events.length === 0
+//       ) {
+//         this.activeDayIsOpen = false;
+//       } else {
+//         this.activeDayIsOpen = true;
+//       }
+//       this.viewDate = date;
+//     }
+//   }
+
+//   eventTimesChanged({
+//     event,
+//     newStart,
+//     newEnd,
+//   }: CalendarEventTimesChangedEvent): void {
+//     this.events = this.events.map((iEvent) => {
+//       if (iEvent === event) {
+//         return {
+//           ...event,
+//           start: newStart,
+//           end: newEnd,
+//         };
+//       }
+//       return iEvent;
+//     });
+//     this.handleEvent('Dropped or resized', event);
+//   }
+
+//   handleEvent(action: string, event: CalendarEvent): void {
+//     this.modalData = { event, action };
+//     this.modal.open(this.modalContent, { size: 'lg' });
+//   }
+
+//   addEvent(): void {
+//     this.events = [
+//       ...this.events,
+//       {
+//         title: 'New event',
+//         start: startOfDay(new Date()),
+//         end: endOfDay(new Date()),
+//         color: colors.red,
+//         draggable: true,
+//         resizable: {
+//           beforeStart: true,
+//           afterEnd: true,
+//         },
+//       },
+//     ];
+//   }
+
+//   deleteEvent(eventToDelete: CalendarEvent) {
+//     this.events = this.events.filter((event) => event !== eventToDelete);
+//   }
+
+//   setView(view: CalendarView) {
+//     this.view = view;
+//   }
+
+//   closeOpenMonthViewDay() {
+//     this.activeDayIsOpen = false;
+//   }
+// }
