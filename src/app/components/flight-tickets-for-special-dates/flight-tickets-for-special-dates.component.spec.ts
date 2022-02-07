37da0b1f01bab221dd25bdf6089542762018d2bc
RomanMaruk <maruchok2@gmail.com>
2022-02-07 20:09:49 +0200

    begin write register compomponent

--- src/app/components/flight-tickets-for-special-dates/flight-tickets-for-special-dates.component.spec.ts
+++ src/app/components/flight-tickets-for-special-dates/flight-tickets-for-special-dates.component.spec.ts
@@ -1,6 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FlightTicketsForSpecialDatesComponent } from './flight-tickets-for-special-dates.component';
+
+import { BrowserModule, By } from '@angular/platform-browser';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgxsModule, Store } from '@ngxs/store';
+import { appState } from 'src/app/store/appState';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { RequestDataState } from 'src/app/store/request-data.state';
+import { Subject } from 'rxjs';
+import { FlightsInfoService } from 'src/app/services/flights-info.service';
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
+import { RequestDataService } from 'src/app/services/request-data.service';
+import { FlightInfoState } from 'src/app/store/flight-info.state';
+import { GetTiketsForSpecialDate } from 'src/app/store/flight-info.action';
+
 
 describe('FlightTicketsForSpecialDatesComponent', () => {
   let component: FlightTicketsForSpecialDatesComponent;
@@ -8,7 +39,36 @@ describe('FlightTicketsForSpecialDatesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FlightTicketsForSpecialDatesComponent ]
+      declarations: [ FlightTicketsForSpecialDatesComponent ],
+      imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatAutocompleteModule,
+        MatInputModule,
+        MatButtonModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        MatTabsModule,
+        MatFormFieldModule,
+        MatTooltipModule,
+        MatIconModule,
+        MatTableModule,
+        MatButtonModule,
+        MatCardModule,
+        MatProgressSpinnerModule,
+        MatToolbarModule,
+        NgxsModule.forRoot(appState, {
+          developmentMode: true,
+        }),
+        NgxsLoggerPluginModule.forRoot(),
+      ],
     })
     .compileComponents();
   });
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm(): void {
+    console.log('initializeForm', this.form)
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
 }
