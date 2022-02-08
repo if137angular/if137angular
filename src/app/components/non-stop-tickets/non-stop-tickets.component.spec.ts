59c24392863aa6cf6c491f1c32ee1b86dcdf424b
Petsoliak <iphahaha@gmail.com>
2022-02-08 03:05:26 +0200

    refactoring

--- src/app/components/non-stop-tickets/non-stop-tickets.component.spec.ts
+++ src/app/components/non-stop-tickets/non-stop-tickets.component.spec.ts
@@ -1,100 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Subject } from 'rxjs';
 import { NgxsModule, Store } from '@ngxs/store';
-import { FlightsInfoService } from 'src/app/services/flights-info.service';
 import { NonStopTicketsComponent } from './non-stop-tickets.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { DebugElement } from '@angular/core';
+import { appState } from 'src/app/store/appState';
+import { FlightsInfoService } from 'src/app/services/flights-info.service';
+import { RequestDataState } from 'src/app/store/request-data.state';
+import { RequestDataService } from 'src/app/services/request-data.service';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
+import { of, Subject } from 'rxjs';
+import { FlightInfoState } from 'src/app/store/flight-info.state';
 
-
-
-describe('NonStopTicketsComponent', () => {
+fdescribe('NonStopTicketsComponent', () => {
   let component: NonStopTicketsComponent;
   let fixture: ComponentFixture<NonStopTicketsComponent>;
-  let debugElement: DebugElement;
-  let storeMock: any;
-  let store: any;
+
   let flightsInfoServiceMock: any;
+  let requestDataServiceMock: any;
+  let storeMock: any;
   let formDataSubject = new Subject();
-  let nonStopTicketsSubject = new Subject();
+  let nonStopTicketSubject = new Subject();
+  let RequestDataStateSubject = new Subject();
 
-
-  beforeEach(() => {
-    storeMock = {
-      select: jasmine.createSpy('select')
-        .withArgs(FormData).and.returnValue(formDataSubject.asObservable()),
-    };
-
+  beforeEach(async () => {
     flightsInfoServiceMock = jasmine.createSpy().and.returnValue({});
 
-      TestBed.configureTestingModule({
-        imports: [
-          BrowserModule,
-          AppRoutingModule,
-          FormsModule,
-          ReactiveFormsModule,
-          MatInputModule,
-          MatButtonModule,
-          MatAutocompleteModule,
-          MatSelectModule,
-          MatDatepickerModule,
-          MatNativeDateModule
-        ],
-      declarations: [ NonStopTicketsComponent ],
+    storeMock = {
+      select: jasmine
+        .createSpy('select')
+        .withArgs(RequestDataState.formData)
+        .and.returnValue(formDataSubject.asObservable())
+        .withArgs(FlightInfoState.nonStopTickets)
+        .and.returnValue(nonStopTicketSubject.asObservable())
+        .withArgs(RequestDataState.currency)
+        .and.returnValue(RequestDataStateSubject.asObservable()),
+      selectSnapshot: jasmine.createSpy('selectSnapshot'),
+    };
+
+    await TestBed.configureTestingModule({
+      declarations: [NonStopTicketsComponent],
+      imports: [
+        NgxsModule.forRoot(appState, {
+          developmentMode: true,
+        }),
+      ],
       providers: [
         { provide: Store, useValue: storeMock },
-        { provide: FlightsInfoService, useValue: flightsInfoServiceMock }
+        { provide: FlightsInfoService, useValue: flightsInfoServiceMock },
+        { provide: RequestDataService, useValue: requestDataServiceMock },
       ],
-    })
-    .compileComponents();
- 
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(NonStopTicketsComponent);
-    debugElement = fixture.debugElement;
-    store = TestBed.get(Store);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  afterAll(() => {
-    formDataSubject.complete();
-    nonStopTicketsSubject.complete();
-  })
-
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  describe('#dispatchNonStopTickets', () => {
-    it('should dispatch Non Stop Tickets', () => {
-      const formData = {
-        destinationFrom: {
-          code: 'LWO'
-        },
-        destinationTo: null,
-      } as any;
-
-      flightsInfoServiceMock.requestNonStopTickets(formData);
-
-      expect(store).toHaveBeenCalledWith(({
-        cityOrigin: 'LWO',
-        cityDestination: ''
-      }))
-
-    })
-  })
-
-
-
-
 });
-
-
