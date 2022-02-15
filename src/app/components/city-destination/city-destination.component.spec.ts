88fe7ce8a50f3a1073f011b2cc04e439d5d80cc8
OlehKachurak <olegxpress7@gmail.com>
2022-02-15 20:06:29 +0200

    unit test for popularDestination

--- src/app/components/city-destination/city-destination.component.spec.ts
+++ src/app/components/city-destination/city-destination.component.spec.ts
@@ -1,38 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CityDestinationComponent } from './city-destination.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from '../../app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatSelectModule } from '@angular/material/select';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { NgxsModule, Store } from '@ngxs/store';
-import { appState } from '../../store/app.state';
-import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
-import { Subject } from 'rxjs';
-import { RequestDataState } from '../../store/request-data.state';
-import { FlightInfoState } from '../../store/flight-info.state';
-import { FlightsInfoService } from '../../services/flights-info.service';
-import { RequestDataService } from '../../services/request-data.service';
-import { SpecialOffersComponent } from '../special-offers/special-offers.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { CitiesModel } from '../../models/cities.model';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {CityDestinationComponent} from './city-destination.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from '../../app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {NgxsModule, Store} from '@ngxs/store';
+import {appState} from '../../store/app.state';
+import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
+import {CUSTOM_ELEMENTS_SCHEMA, DebugElement} from '@angular/core';
+import {Subject} from 'rxjs';
+import {RequestDataState} from '../../store/request-data.state';
+import {FlightInfoState} from '../../store/flight-info.state';
+import {FlightsInfoService} from '../../services/flights-info.service';
+import {RequestDataService} from '../../services/request-data.service';
+import {SpecialOffersComponent} from '../special-offers/special-offers.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {CitiesModel} from '../../models/cities.model';
 import {GetCities, SetFormDate} from '../../store/request-data.action';
 import {GetPopularDestinations} from "../../store/flight-info.action";
+import {FlightPriceTrendsComponent} from "../flight-price-trends/flight-price-trends.component";
+import {string} from "@amcharts/amcharts4/core";
 
 describe('CityDestinationComponent', () => {
   let component: CityDestinationComponent;
@@ -42,14 +44,18 @@ describe('CityDestinationComponent', () => {
   let store: any;
   let flightsInfoServiceMock: any;
   let requestDataService: any;
-  let citiesSubject = new Subject();
+  let popularDestinations = new Subject();
+  let currency = new Subject();
 
   beforeEach(() => {
     storeMock = {
       select: jasmine
         .createSpy('select')
-        .withArgs(RequestDataState.cities)
-        .and.returnValue(citiesSubject.asObservable()),
+        .withArgs(RequestDataState.currency)
+        .and.returnValue(currency.asObservable())
+        .withArgs(FlightInfoState.popularDestinations)
+        .and.returnValue(popularDestinations.asObservable()),
+      dispatch: jasmine.createSpy('dispatch'),
       selectSnapshot: jasmine.createSpy('selectSnapshot'),
     };
     flightsInfoServiceMock = jasmine.createSpy().and.returnValue({});
@@ -79,20 +85,21 @@ describe('CityDestinationComponent', () => {
         MatCardModule,
         MatProgressSpinnerModule,
         MatToolbarModule,
-        NgxsModule.forRoot(appState, {
-          developmentMode: true,
-        }),
+        NgxsModule,
         NgxsLoggerPluginModule.forRoot(),
       ],
-      declarations: [CityDestinationComponent],
+      declarations: [FlightPriceTrendsComponent],
       providers: [
-        { provide: Store, useValue: storeMock },
-        { provide: FlightsInfoService, useValue: flightsInfoServiceMock },
-        { provide: RequestDataService, useValue: requestDataService },
+        {provide: Store, useValue: storeMock},
+        {provide: FlightsInfoService, useValue: flightsInfoServiceMock},
+        {provide: RequestDataService, useValue: requestDataService},
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
+    })
+      .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(CityDestinationComponent);
     debugElement = fixture.debugElement;
     store = TestBed.get(Store);
@@ -101,47 +108,67 @@ describe('CityDestinationComponent', () => {
   });
 
   afterAll(() => {
-    citiesSubject.complete();
+    currency.complete();
+    popularDestinations.complete();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // describe('#ngOninit',() =>{
- //   it('should dispatch GetPopularDestination with appropriate params',() => {
- //     component.ngOnInit();
- //     expect(store.dispatch).toHaveBeenCalledWith(
- //       new GetPopularDestinations({
- //         this.popularDestinationCities
- //       })
- //     )
- //   })
- // } )
 
-  describe('# selectDestination',() => {
-    it('#should dispatch  SetFormDate with appropriate params', () => {
-      component.selectedCities = 'LWO';
-      component.selectedDestinstion = 'WAW';
-      const formData = {
-        destinationFrom: {
-          name: 'Lviv',
-          code: 'LWO',
-        },
-        destinationTo:{
-          name: 'Warsaw',
-          code: 'WAW',
-        },
-        endDate: new Date(),
-        startDate: new Date(),
-      } as any;
+  describe('#ngOnInit', () => {
+    beforeEach(() => {
+      currency.next({
+        code: "WAW",
+        name: "Warsaw",
+      });
+    });
+    describe('#ngOninit',() =>{
+      it('should dispatch GetPopularDestination with appropriate params',() => {
+        component.ngOnInit();
+        expect(store.dispatch).toHaveBeenCalledWith(
+          new GetPopularDestinations(['IEV', 'LWO', 'DNK', 'ODS']
+          )
+        )
+      })
+    })
+  });
 
-      component.selectDestination(formData);
+   describe('# selectDestination', () => {
+      it('#should dispatch  SetFormDate with appropriate params', () => {
+        component.selectedCities = 'LWO';
+        component.selectedDestinstion = 'WAW';
+        const formData = {
+          destinationFrom: {
+            name: 'Lviv',
+            code: 'LWO',
+          },
+          destinationTo: {
+            name: 'Warsaw',
+            code: 'WAW',
+          },
+          endDate: new Date(2022, 1, 11),
+          startDate: new Date(2022, 1, 13),
+        } as any;
 
-      expect(store.dispatch).toHaveBeenCalledWith(
-        new SetFormDate(formData)
-      );
+        component.selectDestination(formData);
+
+        expect(store.dispatch).toHaveBeenCalledWith(
+          new SetFormDate({
+            destinationFrom: {
+              name: 'Lviv',
+              code: 'LWO',
+            },
+            destinationTo: {
+              name: 'Warsaw',
+              code: 'WAW',
+            },
+            endDate: new Date(2022, 1, 11),
+            startDate: new Date(2022, 1, 13),
+          })
+        );
+      });
     });
   });
 
-});
